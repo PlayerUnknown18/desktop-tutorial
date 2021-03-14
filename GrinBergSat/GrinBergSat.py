835e873312bde3ea2e31170c94d892cb734fc28e
תחנת קרקע <adam.funzone@gmail.com>
2021-03-14 14:40:25 +0000

    fix
    fix

--- GrinBergSat/GrinBergSat.py
+++ GrinBergSat/GrinBergSat.py
@@ -10,27 +10,34 @@ import requests
 import json
 import os
 
+
 class DoplerCorrection:
-    def __init__(self,jinfo_object,device):
+    def __init__(self, jinfo_object, device):
         self.config_data = jinfo_object
         self.satellite_freq = float(jinfo_object.hdsdr_freq)
         self.rfcb_freq = float(jinfo_object.rfcb_freq)
         self.__speed_of_light = 299792458
         if device == "hdsdr":
             self.port = jinfo_object.hdsdr_port
-            self.port_comm = port_communication.DoplerPortCommunication(self.port, self.satellite_freq)
+            self.port_comm = port_communication.DoplerPortCommunication(
+                self.port, self.satellite_freq)
 
         self.__offset = jinfo_object.offset
         self.__flip_dopler_number = -1
         self.satellite_name = get_sat_name(int(self.config_data.norad_id))
         if jinfo_object.tle_type == "file":
-            self.doppler_satellite = self.load_tle_file("tle.txt")
+            self.doppler_satellite = self.load_tle_file(r"C:\Users\GroundStation\Documents\GitHub\desktop-tutorial\GrinBergSat\tle.txt")
         else:
-            self.doppler_satellite = load.tle("https://celestrak.com/NORAD/elements/active.txt",reload=False)[self.satellite_name]
-        
-        self.dopler_station = Topos(jinfo_object.station_lat,jinfo_object.station_lon)
+            self.doppler_satellite = load.tle(
+                "https://celestrak.com/NORAD/elements/active.txt", reload=False)[self.satellite_name]
 
-    def calculate_dopler(self,freq,offset,mode):
+        self.dopler_station = Topos(
+            jinfo_object.station_lat, jinfo_object.station_lon)
+
+    def load_tle_file(self,filename):
+        return load.tle_file(filename)[0]
+
+    def calculate_dopler(self, freq, offset, mode):
         timescale = load.timescale(builtin=True)
         t = timescale.utc(datetime.datetime.utcnow().replace(tzinfo=utc))
         t1 = timescale.utc(t.utc_datetime() + datetime.timedelta(seconds=1))
@@ -40,32 +47,33 @@ class DoplerCorrection:
         range2 = diff1.distance().km
         change = (range1 - range2) * 1000
         if mode == "uplink":
-            dopler_corr = (freq * (self.__speed_of_light - change) / self.__speed_of_light) - offset
+            dopler_corr = (freq * (self.__speed_of_light -
+                                   change) / self.__speed_of_light) - offset
         elif mode == "downlink":
-            dopler_corr = (freq * (self.__speed_of_light + change) / self.__speed_of_light) - offset
+            dopler_corr = (freq * (self.__speed_of_light +
+                                   change) / self.__speed_of_light) - offset
         else:
             raise "line 43"
         return dopler_corr
 
-
     def update_dopler_hdsdr(self):
         while True:
             time.sleep(0.3)
-            dopler_freq = self.calculate_dopler(self.satellite_freq,self.__offset,"downlink")
+            dopler_freq = self.calculate_dopler(
+                self.satellite_freq, self.__offset, "downlink")
             print(dopler_freq)
             self.port_comm.write_dopler_corr_to_port(str(dopler_freq))
-
 
     def rfcb_correction_starter(self):
         sock_io = connect_to_sock()
         update_modulation(sock_io)
         self.update_dopler_rfcb(sock_io)
 
-    def update_dopler_rfcb(self,sock_io):
+    def update_dopler_rfcb(self, sock_io):
         get_freq = float(self.rfcb_freq)
         while True:
             freq = get_freq
-            data = [7,13,0,0,0]
+            data = [7, 13, 0, 0, 0]
             data = bytearray(data)
             i = 0
             while i < 8 or freq > 0:
@@ -73,9 +81,9 @@ class DoplerCorrection:
                 freq = int(freq / 256)
                 i += 1
             try:
-                #update_modulation(sock_io)
+                # update_modulation(sock_io)
                 sock_io.send(data)
-                #print(get_freq)
+                # print(get_freq)
             except:
                 print("except")
                 try:
@@ -87,12 +95,15 @@ class DoplerCorrection:
                     time.sleep(5)
                     pass
 
-            get_freq = self.calculate_dopler(self.rfcb_freq, 0, "uplink") * 10 ** 6   #145.970 -> 145970
+            get_freq = self.calculate_dopler(
+                self.rfcb_freq, 0, "uplink") * 10 ** 6  # 145.970 -> 145970
             time.sleep(2)
+
+    
 
 
 class UpdateSatelliteCords:
-    def __init__(self,jinfo_object):
+    def __init__(self, jinfo_object):
         self.config_data = jinfo_object
         self.port = jinfo_object.antenna_port
         self.time_for_tuning_antennas = jinfo_object.time_for_tuning_antennas
@@ -102,14 +113,16 @@ class UpdateSatelliteCords:
         self.__elevation_to_radians_number = 180
         self.satellite_name = get_sat_name(int(self.config_data.norad_id))
         if jinfo_object.tle_type == "file":
-            self.satellite = self.load_tle_file("tle.txt")
+            self.satellite = self.load_tle_file(r"C:\Users\GroundStation\Documents\GitHub\desktop-tutorial\GrinBergSat\tle.txt")
         else:
-            self.satellite = load.tle("https://celestrak.com/NORAD/elements/active.txt",reload=False)[self.satellite_name]
-        
+            self.satellite = load.tle(
+                "https://celestrak.com/NORAD/elements/active.txt", reload=False)[self.satellite_name]
+
         self.station_lon = jinfo_object.station_lon
         self.station_lat = jinfo_object.station_lat
         self.station_alt = jinfo_object.station_elev
-        self.station = Topos(jinfo_object.station_lat,jinfo_object.station_lon)
+        self.station = Topos(jinfo_object.station_lat,
+                             jinfo_object.station_lon)
 
     def check_if_real_cords(self, azimute, elevation):
         if azimute > 0 and elevation > 0:
@@ -117,29 +130,34 @@ class UpdateSatelliteCords:
         else:
             return False
 
+    def load_tle_file(self,filename):
+        return load.tle_file(filename)[0]
+
     def update_satellite_cords(self):
         last_elevation = 0
         last_azimuth = 0
         while True:
             time.sleep(self.time_for_tuning_antennas)
             utc_time_now = datetime.datetime.utcnow()
-            orbital_object = pyorbital.orbital.Orbital(self.satellite_name,"active.txt")
-            self.azimuth,self.elevation = orbital_object.get_observer_look(utc_time_now,self.station_lon,self.station_lat,self.station_alt)
+            orbital_object = pyorbital.orbital.Orbital(
+                self.satellite_name, "active.txt")
+            self.azimuth, self.elevation = orbital_object.get_observer_look(
+                utc_time_now, self.station_lon, self.station_lat, self.station_alt)
             print(f"satellite azimuth now:{self.azimuth}")
             print(f"satellite elevation now:{self.elevation}")
             if int(last_azimuth) != int(self.azimuth) or int(last_elevation) != int(self.elevation):
                 last_elevation = self.elevation
                 last_azimuth = self.azimuth
                 print(self.port_comm.read_antena_coordinates_from_port())
-                if self.check_if_real_cords(self.azimuth,self.elevation):
-                    self.azimuth,self.elevation = self.port_comm.convert_to_format(self.azimuth,self.elevation)
-                    self.port_comm.write_antena_coordinates_to_port(self.azimuth,self.elevation)
-                    
-
+                if self.check_if_real_cords(self.azimuth, self.elevation):
+                    self.azimuth, self.elevation = self.port_comm.convert_to_format(
+                        self.azimuth, self.elevation)
+                    self.port_comm.write_antena_coordinates_to_port(
+                        self.azimuth, self.elevation)
 
 
 class UpdateSatelliteCordsThread(threading.Thread):
-    def __init__(self,jinfo_object):
+    def __init__(self, jinfo_object):
         threading.Thread.__init__(self)
         self.jinfo_object = jinfo_object
 
@@ -149,53 +167,60 @@ class UpdateSatelliteCordsThread(threading.Thread):
 
 
 def update_modulation(sock_io):
-    FSK_CODE = [9, 19, 0, 0, 0, 9, 70, 83, 75, 45, 71, 51, 82, 85, 72, 128, 37, 0, 0]
+    FSK_CODE = [9, 19, 0, 0, 0, 9, 70, 83, 75,
+                45, 71, 51, 82, 85, 72, 128, 37, 0, 0]
     FSK_CODE = bytearray(FSK_CODE)
     sock_io.send(FSK_CODE)
-
 
 
 def connect_to_sock():
     socket_conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     while True:
         try:
-            socket_conn.connect(("127.0.0.1",4532))
+            socket_conn.connect(("127.0.0.1", 4532))
             print("connected to rfcb")
             break
         except:
-            print("cant connect to rfcb,please run the rf checkout box\rtry again in 5 seconds")
+            print(
+                "cant connect to rfcb,please run the rf checkout box\rtry again in 5 seconds")
             time.sleep(5)
     return socket_conn
 
 
 def get_sat_name(norad):
-    satellite = requests.get(f"https://celestrak.com/satcat/tle.php?CATNR={norad}").text
+    satellite = requests.get(
+        f"https://celestrak.com/satcat/tle.php?CATNR={norad}").text
     satellite = satellite[:satellite.find("\n")-1].strip()
     return satellite
 
+
 """delete active.txt(contain the TLE of the satellites)"""
+
+
 def delete_active_file():
     os.remove("active.txt")
 
+
 def main():
-    #loads data from the config file
+    # loads data from the config file
     load_json_object = jsonloader.JsonLoad()
     json_info = load_json_object.return_jinfo_object()
 
     if json_info.rfcb_flag.lower() == "true":
-        dp = DoplerCorrection(json_info,"rfcb")
+        dp = DoplerCorrection(json_info, "rfcb")
         rfcbDpThread = threading.Thread(target=dp.rfcb_correction_starter)
         rfcbDpThread.start()
 
     if json_info.hdsdr_flag.lower() == "true":
-        #can be more efficient by adding one more if
-        dp_hdsdr = DoplerCorrection(json_info,"hdsdr")
+        # can be more efficient by adding one more if
+        dp_hdsdr = DoplerCorrection(json_info, "hdsdr")
         hdsdrDpThread = threading.Thread(target=dp_hdsdr.update_dopler_hdsdr)
         hdsdrDpThread.start()
 
     if json_info.antenna_flag.lower() == "true":
         ant = UpdateSatelliteCordsThread(json_info)
         ant.start()
+
 
 if __name__ == '__main__':
     main()
--- GrinBergSat/jsonloader.py
+++ GrinBergSat/jsonloader.py
@@ -2,7 +2,7 @@ import json
 
 
 class Jinfo:
-    def __init__(self,json_data):
+    def __init__(self, json_data):
         self.hdsdr_freq = json_data['configs']['hdsdr_freq']
         self.rfcb_freq = json_data['configs']['rfcb_freq']
         self.hdsdr_port = json_data['configs']['hdsdr_port']
@@ -10,18 +10,19 @@ class Jinfo:
         self.station_lat = float(json_data['configs']['lat'])
         self.station_elev = float(json_data['configs']['elev'])
         self.offset = float(json_data['configs']['offset'])
-        self.time_for_tuning_antennas = int(json_data['configs']['Time_for_tuning_antennas'])
+        self.time_for_tuning_antennas = int(
+            json_data['configs']['Time_for_tuning_antennas'])
         self.antenna_port = json_data['configs']['antenna_port']
         self.norad_id = json_data['configs']['norad_id']
         self.rfcb_flag = json_data['configs']['rfcb']
         self.hdsdr_flag = json_data['configs']['hdsdr']
         self.antenna_flag = json_data['configs']['antenna']
-
+        self.tle_type = json_data['configs']['tle_type']
 
 
 class JsonLoad:
     def __init__(self):
-        with open("config.json","r") as config_file:
+        with open("config.json", "r") as config_file:
             json_data = json.loads(config_file.read())
         self.json_info = Jinfo(json_data)
 
